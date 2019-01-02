f03c646fbee94c035f84dbf2eea5e3f2e71e1964
ezio84 <brabus84@gmail.com>
2019-01-02 22:25:12 +0100

    Lockscreen items/clocks: KeyguardSliceView improvements/cleanup
    don't check LOCKSCREEN_INFO more times but refresh its value from StatusBar observer
    
    setDarkAmount gets called a lot of times (and consecutively in the same
    keyguard session to get different dark float values), so instead of calling
    showSlice() (which does a lot of stuff) there we can just set the needed
    visibility for the slice row.
    
    fix mRow.setVisibility call in showSlice (it wasn't handling the case where
    no items are available, in that case as per original aosp code the row must
    be set to GONE).
    
    Change-Id: I76d7e9010d5681a4d0e7bc1dc20475844c5e154d

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -1057,6 +1057,7 @@ public class KeyguardStatusView extends GridLayout implements
 
     public void updateAll() {
         updateSettings();
+        mKeyguardSlice.updateSettings();
         mKeyguardSlice.refresh();
     }
 
